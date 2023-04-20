fe5717b908350557d712b05e12195754ab3b838d
Camilo Valdes <bolivar014@gmail.com>
2023-04-20 16:39:17 -0500

    creando cliente de conexión a supabase - Variables de entorno

---
+++ src/supabase/client.js
@@ -0,0 +1,8 @@
+// Importamos client supabase
+import { createClient } from '@supabase/supabase-js'
+
+// Exportamos constante client de supabase
+export const client = createClient(
+    process.env.REACT_APP_SUPABASE_URL, 
+    process.env.REACT_APP_SUPABASE_ANON_KEY
+);
