73a0cb795fdb8f5119869d782b66308a368f3128
Camilo Valdes <bolivar014@gmail.com>
2023-04-20 16:54:49 -0500

    creando eventos para el formulario Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 // Inicializamos función login
 function Login() {
+    // Inicializamos variables donde almacenare el email por medio de evento onchange
+    const [email, setEmail] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Imprimimos...
+        console.log('Email: ' + email);
+    }
+
+    // Retornamos componente login
     return (
-        <div>Login</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="yourEmail@site.com"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <button>Send</button>
+            </form>
+        </div>
     )
 }
 
