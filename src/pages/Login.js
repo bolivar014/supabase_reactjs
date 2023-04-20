9a072159061fe5dc798a0f675515c899a42f5734
Camilo Valdes <bolivar014@gmail.com>
2023-04-20 17:12:18 -0500

    ajustando configuración a servidor local

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,33 @@
 import { useState } from "react"
-
+import { client }  from "../supabase/client";
 // Inicializamos función login
 function Login() {
     // Inicializamos variables donde almacenare el email por medio de evento onchange
     const [email, setEmail] = useState("");
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         // Imprimimos...
         console.log('Email: ' + email);
+
+        // ejecutamos petición asincrona
+        try
+        {
+            const result = await client.auth.signInWithOtp({
+                email,
+                options: {
+                  emailRedirectTo: 'https://www.facebook.com',
+                },
+            });
+            console.log('result:');
+            console.log(result);
+        }
+        catch(error)
+        {
+            console.log('error:');
+            console.error(error);
+        }
     }
 
     // Retornamos componente login
