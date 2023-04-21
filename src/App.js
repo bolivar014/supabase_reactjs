c4e9e740d19c9748e2cd672d65311a8b2bae2c67
Camilo Valdes <bolivar014@gmail.com>
2023-04-20 21:53:41 -0500

    integrando React-router-dom y control de rutas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 // imports
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+
 // Importamos modulo Login
 import Login from "./pages/Login";
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 
 function App() {
   return (
     <div className="App">
-      <Login/> 
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/*' element={<NotFound />}/>
+      </Routes>
     </div>
   );
 }
